e4bd7847c94fd570adf4c4a4260430161321209a
Elina Ojanen <elina.ojanen@pp.inet.fi>
2025-05-05 16:18:07 +0300

    Sovitetaan repon Nuben palvelimeen

--- eljuja/eljuja/settings.py
+++ eljuja/eljuja/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#m33769$a&!fo@&g)qjl%rotq!-lik4%!i=h=nf+gp(7+humws
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_ROOT = BASE_DIR / '..' / 'staticfiles'
