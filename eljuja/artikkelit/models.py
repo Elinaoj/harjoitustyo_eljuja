9431b5a4f9ee30f9003e8c190009c5754fe61a76
Elina Ojanen <elina.ojanen@pp.inet.fi>
2025-06-02 22:33:40 +0300

    Taloyhtion ja asuntojen valinnat

--- eljuja/artikkelit/models.py
+++ eljuja/artikkelit/models.py
@@ -7,6 +7,7 @@ class Artikkeli(models.Model):
     artikkeli = models.CharField(max_length=75)
     hinta = models.DecimalField(default=0, decimal_places=2, max_digits=4)
     lisatietoja = models.CharField(max_length=200, blank=True)
+    lisatietoja = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.artikkeli
