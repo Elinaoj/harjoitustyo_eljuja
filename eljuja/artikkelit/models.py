9c1998cd2376d3c86c4082f94a0472429aa2e18e
Elina Ojanen <elina.ojanen@pp.inet.fi>
2025-06-04 19:08:21 +0300

    Alasvetovalikot 1

--- eljuja/artikkelit/models.py
+++ eljuja/artikkelit/models.py
@@ -1,19 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import ModelForm
-
 
 class Artikkeli(models.Model):
     artikkeli = models.CharField(max_length=75)
     hinta = models.DecimalField(default=0, decimal_places=2, max_digits=4)
-    lisatietoja = models.CharField()
+    lisatietoja = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.artikkeli
     
     class Meta:
         verbose_name_plural = 'Artikkelit'
-
+        managed = True
 
 class Aika(models.Model):
     aika = models.CharField(unique=True, max_length=5, blank=False) 
@@ -23,15 +21,33 @@ class Aika(models.Model):
 
     class Meta:
         verbose_name_plural = 'Ruoka-ajat'
+        managed = True
 
+class Taloyhtio(models.Model):
+    taloyhtio = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Taloyhtiot'
+        managed = True
+
+class Asunto(models.Model):
+    asunto = models.CharField(max_length=5, blank=True)
+    taloyhtio = models.ForeignKey(Taloyhtio, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Asunnot'
+        managed = True
 
 class Myynti(models.Model):
     artikkeli = models.ForeignKey(Artikkeli, on_delete=models.CASCADE)
     kpl = models.IntegerField(blank=True)
     aika = models.ForeignKey(Aika, on_delete=models.CASCADE, null=True, blank=True)
+    taloyhtio = models.ForeignKey(Taloyhtio, on_delete=models.CASCADE, null=True, blank=True)
+    asunto = models.ForeignKey(Asunto, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Myynnit'
+        managed = True
 
 #class Node(models.Model):
     #artikkeli_id = models.CharField(primary_key=True)
